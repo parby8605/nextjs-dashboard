f3e5c738fe4b6c5f8094b8c257e5cbbbe875cc43
parby8605 <lhj971011@gmail.com>
2025-02-25 17:27:58 +0900

    chapter11 - router, searchParam

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,28 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  //debouncing을 통해 300ms 이내에 이벤트가 재발생하지 않으면 실행되도록 최적화
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
+    //input창 값 있으면 URL 쿼리문에 query=asdf 식으로 추가, 없으면 query= 부분 삭제
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    //onChange에서 실행되기 떄문에, input창에 입력하면 url 쿼리문 변경되는거 확인 가능
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +31,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        //기본 입력값을 params에서 가져오기
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
