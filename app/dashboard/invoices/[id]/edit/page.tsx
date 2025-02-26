a2737aa93d1a85d8ed5d84f7b96c956b20d8a609
parby8605 <lhj971011@gmail.com>
2025-02-27 02:47:05 +0900

    chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -9,6 +10,11 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    //404일때 라우팅 경로의 not-found.tsx로 이동하는 메서드, error.tsx보다 우선순위가 높음
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
